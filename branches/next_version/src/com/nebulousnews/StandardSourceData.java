4da09bcf3694e80eab08ef8dc4b403896daecd87
trafficone <trafficone@f215413a-1ca8-11de-b6aa-3535bc3264a2>
2009-06-09 19:45:31 +0000

    some code updated, could not complete, fixing other issues simultaneously

--- branches/next_version/src/com/nebulousnews/StandardSourceData.java
+++ branches/next_version/src/com/nebulousnews/StandardSourceData.java
@@ -1,38 +1,57 @@
 package com.nebulousnews;
 
+import java.io.DataInput;
+import java.io.IOException;
 
-abstract public class StandardSourceData {
+
+abstract public class StandardSourceData implements Writable{
 	private String uID;
-	private int dataType;
+	private String dataType;
+	private HashMap metadata;
+	private byte[] data;
 	
 	public String UID(){
-		this.uid = "";
+		this.uID = "";
 		this.dataType = 0;
 	}
 	
-	public String UID(String uID) {
-		this.uID = uID;
+	public String UID(String uid) {
+		this.uID = uid;
 		this.dataType = 0;
 	}
 	
-	public int setDataType(int dataType){
-		this.dataType = dataType;
+	public int setDataType(int datatype){
+		this.dataType = datatype;
 	}
 
-	abstract public setData(T data){
+	public void write(DataOutput out){
+		out.write("STD|"+uID+"|"+dataType+"|");
+		out.write(data.length+"|");
+		out.write(data);
+		out.write("|");
+		out.write(metadata.toString()+"\n");
 		
 	}
 	
-	abstract public <T> getData(){
-		
+	public void readFields(DataInput in) throws IOException {
+		String in.readLine()
 	}
 	
-	abstract public setMetaData(T metaData){
-		
+	abstract public setData(byte[] dataIn){
+		data = dataIn;
 	}
 	
-	abstract public <T> getMetaData(){
-		
+	abstract public byte[] getData(){
+		return data;
 	}
+	
+	abstract public setMetaData(HashMap metaData){
+		metaData = metaData;
+	}
+	
+	abstract public HashMap getMetaData(){
+		return metadata
+	}
+	
 	
 }
